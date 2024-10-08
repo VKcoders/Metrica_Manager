83fca630d5302a8954f5de0c031fc7975db02006
Vincenzofdg <vincenzofdg.dev@gmail.com>
2024-10-08 08:58:36 -0300

    feat: renamed

--- ios/metrica_manager/AppDelegate.mm
+++ ios/metrica_manager/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"metrica_manager";
+  self.moduleName = @"com.app_manager";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
--- ios/metrica_managerTests/metrica_managerTests.m
+++ ios/metrica_managerTests/metrica_managerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface metrica_managerTests : XCTestCase
+@interface comappmanagerTests : XCTestCase
 
 @end
 
-@implementation metrica_managerTests
+@implementation comappmanagerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
